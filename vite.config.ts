0239da13dbc2aceff810eb5ec8ecec5d57d72dd3
Ismael Costa <ixmawel3@gmail.com>
2026-08-01 14:32:53 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 // Set `base` to your repository name when deploying to GitHub Pages.
 // Replace `EnglishBooks` below if your repository has a different name.
 export default defineConfig({
-  // Base set for GitHub Pages. Repository: ixmawel3-hub/AEF4E3
-    // Use repo base for GitHub Pages builds (GH_PAGES or running on GitHub Actions),
-    // otherwise use '/' for the dev server so localhost works.
-    base: (process.env.GH_PAGES) ? '/AEF4E3/' : '/',
+  base: (process.env.GH_PAGES) ? '/AEF3-4th-Edition/' : '/',
   plugins: [react()],
 })
