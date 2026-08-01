5a0e546e67c6bca23583934e5058121c3d59fab1
Ismael Costa <ixmawel3@gmail.com>
2026-08-01 17:17:23 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// Set `base` to your repository name when deploying to GitHub Pages.
-// Replace `EnglishBooks` below if your repository has a different name.
 export default defineConfig({
-  base: (process.env.GH_PAGES) ? '/AEF4E3/' : '/',
+  base: (process.env.GH_PAGES) ? '/AEF3-4th-Edition/' : '/',
   plugins: [react()],
 })
