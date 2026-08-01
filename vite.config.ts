48296598cf148165ebcf5c0bc1ef9fe9c7afbfeb
Ismael Costa <ixmawel3@gmail.com>
2026-08-01 17:19:41 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: (process.env.GH_PAGES) ? '/AEF3-4th-Edition/' : '/',
+  base: ('/AEF3-4th-Edition/'),
   plugins: [react()],
 })
