665dae43755b47a83a029bbee2e7c47477dfebd8
Ismael Costa <ixmawel3@gmail.com>
2026-08-01 18:04:03 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// Set `base` to your repository name when deploying to GitHub Pages.
-// Replace `EnglishBooks` below if your repository has a different name.
 export default defineConfig({
-  base: ('./'),
+  base: ('/'),
   plugins: [react()],
 })
