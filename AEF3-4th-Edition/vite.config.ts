50b1469099469fc42c6982a2ca40a7eca7cd7d86
Ismael Costa <ixmawel3@gmail.com>
2026-08-01 14:48:10 -0300

    first commit

---
+++ AEF3-4th-Edition/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/AEF3-4th-Edition/',
+  plugins: [react()],
+})
