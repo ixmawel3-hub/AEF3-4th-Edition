6fbe845a2dd0d0614af7176c540a4b69633565bf
Ismael Costa <ixmawel3@gmail.com>
2026-08-01 18:12:33 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: ('/'),
+  base: ('/AEF3-4th-Edition'),
   plugins: [react()],
 })
